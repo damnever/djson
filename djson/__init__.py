7d723eeeca6cf4e065e35c2896ee631fe9b479e9
Damnever <dxc.wolf@gmail.com>
2016-07-29 20:14:57 +0800

    bump version

--- djson/__init__.py
+++ djson/__init__.py
@@ -8,7 +8,7 @@ from .encoder import dumps, dump, JSONEncoder
 from .exc import JSONDecodeError, JSONEncodeError
 
 
-version = __version__ = '0.1.0'
+version = __version__ = '0.1.1'
 version_info = [int(num) for num in version.split('.')]
 author = __author__ = 'damnever <dxc.wolf@gmail.com>'
 
